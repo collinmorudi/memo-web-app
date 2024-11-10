cffa4c5d51813c691d78f0428b67fe458d671e46
Collin Morudi <collinmorudi@gmail.com>
2024-11-10 12:39:06 +0200

    removed misplaced package files

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -40,4 +40,43 @@ router.post('/login', async (req, res) => {
     const token = user.generateAuthToken();
     respose = {'token': token, 'email': user.email, 'memos': user.memos};
     return res.send(response);
-});  
+});
+
+// Add memo
+function validateMemo(content) {
+    const schema = joi.object({
+        content: joi.string().required()
+    });
+    return schema.validate(content);
+}
+
+router.post('/add-memo', auth, async (req, res) => {
+    const {error} = validateMemo(req.body);
+    if (error) return res.status(400).send('Invalid memo.');
+    const user = await User.findById(req.user._id);
+    if (!user) return res.status(401).send('Not authorized to perform this action.');
+    user.memos.push({ timesStamp: Date.now(), content: req.body.content });
+    user.save();
+});
+
+
+// Delete memo
+function validateDeleteMemo(req) {
+    const schema = joi.object({
+        index: joi.number().integer().required(),
+    });
+    return schema.validate(req);
+}
+
+router.post('/delete-memo', auth, async(req, res) => {
+    const {error} = validateDeleteMemo(req.body);
+    if (error) return res.status(400).send('Invalid delete memo');
+    const user = await User.findById(req.user._id);
+    if (!user) return res.status(401).send('Not authorized to perform this action.');
+    user.memos.splice(req.body.index, 1);
+    user.save();
+    return res.send(user.memos);
+});
+
+// export
+module.exports = router;
